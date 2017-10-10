cf081e41871fbbf83d13a1a73825ac5735e407c7
Hack Reactor Student <student@hackreactor.com>
2017-10-10 10:38:10 -0700

    current draft

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,14 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  //1. return obj.toString() 
+  if (typeof obj === 'number' || typeof obj === 'boolean') {
+    return obj.toString()
+  } else if (obj === null) {
+    return 'null';
+  } 
+
+  
+
+
 };
