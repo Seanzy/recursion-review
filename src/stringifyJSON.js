b39c72d3fee5509a19f970076277ab82fb8d56a0
Hack Reactor Student <student@hackreactor.com>
2017-10-10 10:52:32 -0700

    "'Hello World'"

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -9,9 +9,36 @@ var stringifyJSON = function(obj) {
     return obj.toString()
   } else if (obj === null) {
     return 'null';
-  } 
+  } else if (typeof obj === 'string') {
+    return("\"" + obj + "\"");
+  }
 
   
 
 
 };
+
+
+// var stringifiableObjects = [
+//   9,
+//   null,
+//   true,
+//   false,
+//   'Hello world',
+//   [],
+//   [8],
+//   ['hi'],
+//   [8, 'hi'],
+//   [1, 0, -1, -0.3, 0.3, 1343.32, 3345, 0.00011999999999999999],
+//   [8, [[], 3, 4]],
+//   [[[['foo']]]],
+//   {},
+//   {'a': 'apple'},
+//   {'foo': true, 'bar': false, 'baz': null},
+//   {'boolean, true': true, 'boolean, false': false, 'null': null },
+//   // basic nesting
+//   {'a': {'b': 'c'}},
+//   {'a': ['b', 'c']},
+//   [{'a': 'b'}, {'c': 'd'}],
+//   {'a': [], 'c': {}, 'b': true}
+// ];
